d35e89f0673dcf9731b686a1fb14116c0212e5fc
Igor2723 <116388804+Igor2723@users.noreply.github.com>
2024-02-22 17:31:42 +0100

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,9 +9,9 @@ function Footer() {
     <footer>
         <h4>Copyright &copy; 2024. All rights reserved</h4>
         <div className="footer-container">
-            <a href="https://github.com/Igor2723" target="_blank"><FontAwesomeIcon className="contact-icon" icon={faGithub} style={{color: "#FFFFFF",}} /></a>
-            <a href="https://www.linkedin.com/in/igor-zivkovic-5846b6252/" target="_blank"><FontAwesomeIcon className="contact-icon" icon={faLinkedin} style={{color: "#FFFFFF",}} /></a>
-            <a href="mailto:igorbussineszivkovic95@gmail.com"><FontAwesomeIcon className="contact-icon" icon={faEnvelope} style={{color: "#FFFFFF",}} /></a>
+            <a href="https://github.com/Igor2723" title="GitHub" target="_blank"><FontAwesomeIcon className="contact-icon" icon={faGithub} style={{color: "#FFFFFF",}} /></a>
+            <a href="https://www.linkedin.com/in/igor-zivkovic-5846b6252/" title="LinkedIn" target="_blank"><FontAwesomeIcon className="contact-icon" icon={faLinkedin} style={{color: "#FFFFFF",}} /></a>
+            <a href="mailto:igorbussineszivkovic95@gmail.com"><FontAwesomeIcon title="Gmail" className="contact-icon" icon={faEnvelope} style={{color: "#FFFFFF",}} /></a>
         </div>
     </footer>
     )
